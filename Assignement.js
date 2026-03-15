9f2c9b54828edec46bbcff317db5896f28909991
Chetan Bachchhav <156205330+chetanpatil45@users.noreply.github.com>
2026-03-15 15:46:03 +0530

    Create Assignement.js

---
+++ Assignement.js
@@ -0,0 +1,10 @@
+let n = 5;
+let num = 1;
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(num + " ");
+        num++;
+    }
+    console.log();
+}
